40b13ed4a7894cdd606fe4ceabf8332059655cb2
wellinton <wellinton1067@gmail.com>
2023-02-28 16:50:33 -0300

    config gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -2,8 +2,33 @@ module.exports = function(grunt) {
     grunt.initConfig({
         sass: {
             development: {
-                
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    'dev/styles/main.css' : 'src/styles/main.scss'
+                }
+            },
+            production: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'dist/styles/main.min.css' : 'src/styles/main.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'src/styles/**/*.scss',
+                tasks: ['sass:development']
             }
         }
-    })
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sass:development','watch']);
+    grunt.registerTask('build', ['sass:production']);
 }
