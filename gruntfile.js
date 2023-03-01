0d52dfc1415fab2e06d685b779e02e8d77fa9e68
wellinton <wellinton1067@gmail.com>
2023-03-01 17:24:56 -0300

    add fonts and hero

--- gruntfile.js
+++ gruntfile.js
@@ -18,6 +18,32 @@ module.exports = function(grunt) {
                 }
             }
         },
+        imagemin: {
+            production: {
+                options: {
+                    optimizationLevel: 3,
+                    progressive: true
+                },   
+                files: [{
+                    expand: true,
+                    cwd: 'src/images/',
+                    src: ['**/*.{png,jpg,jpeg,gif,svg,ico}'],
+                    dest: 'dist/images/'
+                }]
+            },
+            development: {
+                options: {
+                    optimizationLevel: 3,
+                    progressive: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/images/',
+                    src: ['**/*.{png,jpg,jpeg,gif,svg,ico}'],
+                    dest: 'dev/images/'
+                }]
+            }
+        },
         watch: {
             css: {
                 files: 'src/styles/**/*.scss',
@@ -28,7 +54,8 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    grunt.registerTask('default', ['sass:development','watch']);
-    grunt.registerTask('build', ['sass:production']);
+    grunt.registerTask('default', ['sass:development','imagemin:development','watch']);
+    grunt.registerTask('build', ['sass:production','imagemin:production']);
 }
